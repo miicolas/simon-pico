a2040f88f72cdca282c65aa87796b1089f4823de
Aldric S. Vendas <148966307+AS-Ven@users.noreply.github.com>
2025-01-21 11:49:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import ssd1306
 
 # Set the pins
 i2c = SoftI2C(scl=Pin(9), sda=Pin(8)) # Screen
-btn = Pin(3, Pin.IN, Pin.PULL_DOWN) # Button
+btn_A = Pin(15, Pin.IN, Pin.PULL_DOWN) # Button
+btn_B = Pin(14, Pin.IN, Pin.PULL_DOWN) # Button
+btn_C = Pin(13, Pin.IN, Pin.PULL_DOWN) # Button
+btn_D = Pin(12, Pin.IN, Pin.PULL_DOWN) # Button
+btn_E = Pin(3, Pin.IN, Pin.PULL_DOWN) # Button
 
 # Set the screen parameters
 oled_width = 128
@@ -16,8 +20,16 @@ oled.show()
 
 while True :
     oled.fill(0)  # Clear the screen
-    if btn.value() == 1:  # Button is pressed
-        oled.text('Button Pressed!', 0, 0)
+    if btn_A.value() == 1:  # Button is pressed
+        oled.text('Button A Pressed!', 0, 0)
+    elif btn_B.value() == 1:  # Button is pressed
+        oled.text('Button B Pressed!', 0, 0)
+    elif btn_C.value() == 1:  # Button is pressed
+        oled.text('Button C Pressed!', 0, 0)
+    elif btn_D.value() == 1:  # Button is pressed
+        oled.text('Button D Pressed!', 0, 0)
+    elif btn_E.value() == 1:  # Button is pressed
+        oled.text('Button E Pressed!', 0, 0)
     else:
         oled.text('Press the button!', 0, 0)
     oled.show()

bd5fb50ec1e4bd66b6100aa1ec89c01d127fc33e
Aldric S. Vendas <148966307+AS-Ven@users.noreply.github.com>
2025-01-24 12:58:01 +0100

    Random Lights

--- main.py
+++ main.py
@@ -1,55 +1,87 @@
-# Import the libraries
+#region Import
 from machine import Pin, SoftI2C
 import ssd1306
 from neopixel import Neopixel
+import time
+import random
+
+#endregion
+
+
+
+#region Setup
 
 # Set the pins
-i2c = SoftI2C(scl=Pin(9), sda=Pin(8))  # Screen
-btn_A = Pin(15, Pin.IN, Pin.PULL_DOWN)  # Button
-btn_B = Pin(14, Pin.IN, Pin.PULL_DOWN)  # Button
-btn_C = Pin(13, Pin.IN, Pin.PULL_DOWN)  # Button
-btn_D = Pin(12, Pin.IN, Pin.PULL_DOWN)  # Button
-btn_E = Pin(3, Pin.IN, Pin.PULL_DOWN)   # Button
+screen_pin = SoftI2C(scl=Pin(9), sda=Pin(8))
+
+btn_A = Pin(15, Pin.IN, Pin.PULL_DOWN)
+btn_B = Pin(14, Pin.IN, Pin.PULL_DOWN)
+btn_C = Pin(13, Pin.IN, Pin.PULL_DOWN)
+btn_D = Pin(12, Pin.IN, Pin.PULL_DOWN)
+btn_E = Pin(3, Pin.IN, Pin.PULL_DOWN)
+btn_list = [btn_A, btn_B, btn_C, btn_D, btn_E]
+
+led_pin = 20
 
 # Set up WS2812 LEDs
-num_leds = 12  # Number of LEDs in the ring
-led_pin = 20  # Pin connected to the data line of the LED ring
-leds = Neopixel(num_leds,0,led_pin)
+num_leds = 12
+leds = Neopixel(num_leds, 0, led_pin)
 leds.brightness(5)
+
+led_A = 2
+led_B = 4
+led_C = 6
+led_D = 8
+led_E = 10
+led_list = [led_A, led_B, led_C, led_D, led_E]
+
+# Set color
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+color_list = [red, blue, green]
 
 # Set the screen parameters
 screen_width = 128
 screen_height = 64
-screen = ssd1306.SSD1306_I2C(screen_width, screen_height, i2c)
+screen = ssd1306.SSD1306_I2C(screen_width, screen_height, screen_pin)
 
-# Function to light up the first LED
-def light_up_first_led(color):
-    leds.fill((0, 0, 0))
-    leds.set_pixel(0,color)
-    leds.show()
+#endregion
 
-# Main loop
-while True:
-    screen.fill(0)  # Clear the screen
 
-    if btn_A.value() == 1:  # Button is pressed
-        screen.text('Button A Pressed!', 0, 0)
-        light_up_first_led((255, 0, 0))  # Light up the first LED in red
-    elif btn_B.value() == 1:  # Button is pressed
-        screen.text('Button B Pressed!', 0, 0)
-        light_up_first_led((0, 255, 0))  # Light up the first LED in green
-    elif btn_C.value() == 1:  # Button is pressed
-        screen.text('Button C Pressed!', 0, 0)
-        light_up_first_led((0, 0, 255))  # Light up the first LED in blue
-    elif btn_D.value() == 1:  # Button is pressed
-        screen.text('Button D Pressed!', 0, 0)
-        light_up_first_led((255, 255, 0))  # Light up the first LED in yellow
-    elif btn_E.value() == 1:  # Button is pressed
-        screen.text('Button E Pressed!', 0, 0)
-        light_up_first_led((255, 0, 255))  # Light up the first LED in purple
-    else:
-        screen.text('Press the button!', 0, 0)
-        light_up_first_led((0, 0, 0))  # Turn off all LEDs
 
-    screen.show()
+#region Variables
 
+patern = []
+
+#endregion
+
+
+
+#region Functions
+
+def light_random(i):
+    for x in range(i): 
+        leds.clear()
+
+        led_r = random.randrange(0, len(led_list))
+        leds.set_pixel(led_list[led_r], blue)
+
+        leds.show()
+
+        time.sleep(0.5)
+
+        leds.clear()
+        leds.show()
+
+        time.sleep(0.75)
+
+#endregion
+
+
+
+#region Exec
+
+light_random(50)
+
+#endregion

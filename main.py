0aa386665eb803a7a361dc413406e49a42c7e054
Aldric S. Vendas <148966307+AS-Ven@users.noreply.github.com>
2025-01-24 11:02:30 +0100

    Led setup

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
-# Import the library
+# Import the libraries
 from machine import Pin, SoftI2C
 import ssd1306
+from neopixel import Neopixel
 
 # Set the pins
-i2c = SoftI2C(scl=Pin(9), sda=Pin(8)) # Screen
-btn_A = Pin(15, Pin.IN, Pin.PULL_DOWN) # Button
-btn_B = Pin(14, Pin.IN, Pin.PULL_DOWN) # Button
-btn_C = Pin(13, Pin.IN, Pin.PULL_DOWN) # Button
-btn_D = Pin(12, Pin.IN, Pin.PULL_DOWN) # Button
-btn_E = Pin(3, Pin.IN, Pin.PULL_DOWN) # Button
+i2c = SoftI2C(scl=Pin(9), sda=Pin(8))  # Screen
+btn_A = Pin(15, Pin.IN, Pin.PULL_DOWN)  # Button
+btn_B = Pin(14, Pin.IN, Pin.PULL_DOWN)  # Button
+btn_C = Pin(13, Pin.IN, Pin.PULL_DOWN)  # Button
+btn_D = Pin(12, Pin.IN, Pin.PULL_DOWN)  # Button
+btn_E = Pin(3, Pin.IN, Pin.PULL_DOWN)   # Button
+
+# Set up WS2812 LEDs
+num_leds = 12  # Number of LEDs in the ring
+led_pin = 20  # Pin connected to the data line of the LED ring
+leds = Neopixel(num_leds,0,led_pin)
+leds.brightness(5)
 
 # Set the screen parameters
-oled_width = 128
-oled_height = 64
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+screen_width = 128
+screen_height = 64
+screen = ssd1306.SSD1306_I2C(screen_width, screen_height, i2c)
 
-# Show the screen
-oled.show()
+# Function to light up the first LED
+def light_up_first_led(color):
+    leds.fill((0, 0, 0))
+    leds.set_pixel(0,color)
+    leds.show()
 
-while True :
-    oled.fill(0)  # Clear the screen
+# Main loop
+while True:
+    screen.fill(0)  # Clear the screen
+
     if btn_A.value() == 1:  # Button is pressed
-        oled.text('Button A Pressed!', 0, 0)
+        screen.text('Button A Pressed!', 0, 0)
+        light_up_first_led((255, 0, 0))  # Light up the first LED in red
     elif btn_B.value() == 1:  # Button is pressed
-        oled.text('Button B Pressed!', 0, 0)
+        screen.text('Button B Pressed!', 0, 0)
+        light_up_first_led((0, 255, 0))  # Light up the first LED in green
     elif btn_C.value() == 1:  # Button is pressed
-        oled.text('Button C Pressed!', 0, 0)
+        screen.text('Button C Pressed!', 0, 0)
+        light_up_first_led((0, 0, 255))  # Light up the first LED in blue
     elif btn_D.value() == 1:  # Button is pressed
-        oled.text('Button D Pressed!', 0, 0)
+        screen.text('Button D Pressed!', 0, 0)
+        light_up_first_led((255, 255, 0))  # Light up the first LED in yellow
     elif btn_E.value() == 1:  # Button is pressed
-        oled.text('Button E Pressed!', 0, 0)
+        screen.text('Button E Pressed!', 0, 0)
+        light_up_first_led((255, 0, 255))  # Light up the first LED in purple
     else:
-        oled.text('Press the button!', 0, 0)
-    oled.show()
+        screen.text('Press the button!', 0, 0)
+        light_up_first_led((0, 0, 0))  # Turn off all LEDs
+
+    screen.show()
+
